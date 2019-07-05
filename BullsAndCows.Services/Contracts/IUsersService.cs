307c87030a9e3fd090f84d84970eae207d39eda3
volenpopov <volenpopov@abv.bg>
2019-07-05 09:38:04 +0300

    create userServuce

---
+++ BullsAndCows.Services/Contracts/IUsersService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace BullsAndCows.Services.Contracts
+{
+    public interface IUsersService
+    {
+        Task CreateUserAsync();
+    }
+}
---
+++ BullsAndCows.Services/UsersService.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using BullsAndCows.Data;
+using BullsAndCows.Data.Models;
+using BullsAndCows.Services.Contracts;
+using Microsoft.AspNetCore.Identity;
+
+namespace BullsAndCows.Services
+{
+    public class UsersService : IUsersService
+    {
+        private readonly BullsAndCowsDbContext dbContext;
+        private readonly UserManager<BullsAndCowsUser> userManager;
+
+        public UsersService(BullsAndCowsDbContext dbContext, UserManager<BullsAndCowsUser> userManager)
+        {
+            this.dbContext = dbContext;
+            this.userManager = userManager;
+        }
+
+        public Task CreateUserAsync(string username, string password, string confirmPassword)
+        {
+            
+        }
+    }
+}
