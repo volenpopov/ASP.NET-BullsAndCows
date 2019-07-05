898d92e5a887558f1f2da3a58f7ae96782f39662
volenpopov <volenpopov@abv.bg>
2019-07-05 08:27:32 +0300

    add Filter for AutoValidateAntiForgeryToken on Post requests

--- BullsAndCows.Web/Startup.cs
+++ BullsAndCows.Web/Startup.cs
@@ -40,7 +40,11 @@ namespace BullsAndCows
                .AddDefaultTokenProviders()
                .AddEntityFrameworkStores<BullsAndCowsDbContext>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+            })
+            .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
