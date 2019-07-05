b074b43526bea3a2df87423238effc67c46b70c0
volenpopov <volenpopov@abv.bg>
2019-07-05 14:59:07 +0300

    create UserProfileViewModel

---
+++ BullsAndCows.Models/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace BullsAndCows.Models.ViewModels
+{
+    public class UserProfileViewModel
+    {
+        public string Username { get; set; }
+
+        public string CreatedOn { get; set; }
+
+        public int Wins { get; set; }
+
+        public int Losses { get; set; }
+
+        public string WinLossRatio => $"{this.Wins / this.Losses:f1}";
+
+        public int TotalGames => this.Wins + this.Losses;
+        
+        public int TotalPoints { get; set; }
+    }
+}
