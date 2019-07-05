6da840c8f9dbf88c6039175f1edc0b68e0d997a8
volenpopov <volenpopov@abv.bg>
2019-07-05 15:35:55 +0300

    update Display Names of UserProfileViewModel

--- BullsAndCows.Models/ViewModels/UserProfileViewModel.cs
+++ BullsAndCows.Models/ViewModels/UserProfileViewModel.cs
@@ -13,12 +13,15 @@ namespace BullsAndCows.Models.ViewModels
 
         public int Losses { get; set; }
 
+        [Display(Name = "Win/Loss Ratio:")]
         public string WinLossRatio => this.Losses > 0
                                         ? $"{this.Wins / this.Losses:f1}%"
                                         : "0%";
-        
+
+        [Display(Name = "Games played:")]
         public int TotalGames => this.Wins + this.Losses;
-        
+
+        [Display(Name = "Total Points:")]
         public int TotalPoints { get; set; }
     }
 }
