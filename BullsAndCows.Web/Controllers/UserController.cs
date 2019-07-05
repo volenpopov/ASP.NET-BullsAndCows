e511bfa30808dd7a38baf3b4f7840a191f4cff42
volenpopov <volenpopov@abv.bg>
2019-07-05 06:31:10 +0300

    Create User controller

---
+++ BullsAndCows.Web/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BullsAndCows.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
