bfbd6e8c1e4a95003da0e0ec9f90b0bc8b0f91b8
volenpopov <volenpopov@abv.bg>
2019-07-06 21:28:27 +0300

    add migration for Game model and the adjusted user model

--- BullsAndCows.Data/Migrations/20190706182613_AddGameModelAndEditUserModel.Designer.cs
+++ BullsAndCows.Data/Migrations/20190706182613_AddGameModelAndEditUserModel.Designer.cs
@@ -4,14 +4,16 @@ using BullsAndCows.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BullsAndCows.Data.Migrations
 {
     [DbContext(typeof(BullsAndCowsDbContext))]
-    partial class BullsAndCowsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190706182613_AddGameModelAndEditUserModel")]
+    partial class AddGameModelAndEditUserModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BullsAndCows.Data/Migrations/20190706182613_AddGameModelAndEditUserModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BullsAndCows.Data.Migrations
+{
+    public partial class AddGameModelAndEditUserModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Losses",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TotalPoints",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Wins",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateTable(
+                name: "Game",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    Number = table.Column<int>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    UserId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Game", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Game_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Game_UserId",
+                table: "Game",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Game");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Losses",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalPoints",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Wins",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
