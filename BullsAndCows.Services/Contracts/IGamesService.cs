a74fdd155fe7c77f4fa663d58a19acec144c5c02
volenpopov <volenpopov@abv.bg>
2019-07-06 21:31:56 +0300

    add Game service

---
+++ BullsAndCows.Services/Contracts/IGamesService.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BullsAndCows.Services.Contracts
+{
+    public interface IGamesService
+    {
+        Task<int> InitializeGame(ClaimsPrincipal principal);
+    }
+}
---
+++ BullsAndCows.Services/GamesService.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using BullsAndCows.Data;
+using BullsAndCows.Data.Models;
+using BullsAndCows.Services.Contracts;
+using Microsoft.AspNetCore.Identity;
+
+namespace BullsAndCows.Services
+{
+    public class GamesService : IGamesService
+    {
+        private readonly BullsAndCowsDbContext dbContext;
+        private readonly UserManager<BullsAndCowsUser> userManager;
+
+        public GamesService(BullsAndCowsDbContext dbContext, UserManager<BullsAndCowsUser> userManager)
+        {
+            this.dbContext = dbContext;
+            this.userManager = userManager;
+        }
+        public async Task<int> InitializeGame(ClaimsPrincipal principal)
+        {
+            var user = await this.userManager.GetUserAsync(principal);
+
+            var game = new Game
+            {
+                User = user
+            };
+
+            await this.dbContext.Games.AddAsync(game);
+
+            await this.dbContext.SaveChangesAsync();
+
+            return game.Number;
+        }
+    }
+}
