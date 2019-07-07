aa11a9b7c511cb6c7696ee783a4b15a7a7a647a5
volenpopov <volenpopov@abv.bg>
2019-07-07 19:02:11 +0300

    move js delete acc functionality to js file

---
+++ BullsAndCows.Web/wwwroot/js/accountDelete.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    let deleteBtn = document.getElementById("deleteBtn");
+    let confirmedDeleteBtn = document.getElementById("confirmedDelete");
+
+    deleteBtn.addEventListener("click", function () {
+        let confirmation = confirm("Please confirm that you want to delete your account!");
+
+        if (confirmation) {
+            confirmedDeleteBtn.click();
+        }
+    })
+})()
